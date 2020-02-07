1829e52df75d0d4f4fb4ee04f218fb8eca1f1e43
Kakeru Fukuda <stand.by.we@gmail.com>
2020-02-07 19:08:59 +0900

    Initial Commit

---
+++ nomipro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nomipro
+//
+//  Created by 福田走 on 2020/02/07.
+//  Copyright © 2020 福田走. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
